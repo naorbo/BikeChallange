54e60927bbad6420fac22d5a354ee1a528aca444
avihou <shlomi.avihou@gmail.com>
2014-07-17 01:23:36 +0300

    17072014-0 Mail Controller
    Added -
    Mail API
    Minor UI adjustments

---
+++ BikeChallengeApp/Models/MailModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeChallengeApp.Models
+{
+    public class MailModel
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
