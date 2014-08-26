74a60faf744ed22e961d58338183e22407364056
naorbo <naorbo@gmail.com>
2014-08-26 22:07:02 +0300

    26082014-1, Users report

--- BikeChallengeApp/Controllers/ReportController.cs
+++ BikeChallengeApp/Controllers/ReportController.cs
@@ -18,96 +18,141 @@ using Newtonsoft.Json;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace BikeChallengeApp.Controllers
 {
     public class ReportController : ApiController
     {
-        //[{"UserEmail":"Rider@updated. Email","UserDes":"betaTester","UserFname":"עודד","UserLname":"מנשה","UserAddress":"העצמאות 1","UserPhone":"888888","ImagePath":"ProfileImages\\Users\\betaTester\\ProfileImage07_6_2014.jpg","Gender":"M","Captain":false,"BirthDate":"2004-04-04","JoinDate":"2014-05-09","BicycleType":"הרים","RiderCity":"רעננה","GroupName":"פיתוח","GroupDes":"פיתוח","OrganizationName":"Microsoft","OrganizationDes":"Microsoft","OrganiztionImage":"ProfileImages\\Organizations\\Microsoft\\ProfileImage02_6_2014.png","OrgCity":"אבטליון"},
-        //{"UserEmail":"carmela@harim.com","UserDes":"betaTester2","UserFname":"כרמלה","UserLname":"מנשה","UserAddress":"ההסתדרות 10","UserPhone":"054444666","ImagePath":"ProfileImages\\Users\\undefined\\ProfileImage09_5_2014.jpg","Gender":"M","Captain":false,"BirthDate":"1970-11-05","JoinDate":"2014-05-09","BicycleType":"הרים","RiderCity":"אבני חפץ","GroupName":"SecondGroup","GroupDes":"SecondGroup","OrganizationName":"EBAY","OrganizationDes":"EBAY","OrganiztionImage":"ProfileImages\\Organizations\\EBAY\\ProfileImage02_6_2014.png","OrgCity":"אבטליון"}]
-        // [{"FirstCol":"A1","SecondCol":"A2","ThirdCol":"A3"},{"FirstCol":"B1","SecondCol":"B2","ThirdCol":"B3"}]
-        //public HttpResponseMessage Post(int ColNum, string type, List<Report> mlist )
+        /*
+        [{"UserEmail":"Rider@updated. Email","UserDes":"betaTester","UserFname":"עודד","UserLname":"מנשה","UserAddress":"העצמאות 1","UserPhone":"888888","ImagePath":"ProfileImages\\Users\\betaTester\\ProfileImage07_6_2014.jpg","Gender":"M","Captain":false,"BirthDate":"2004-04-04","JoinDate":"2014-05-09","BicycleType":"הרים","RiderCity":"רעננה","GroupName":"פיתוח","GroupDes":"פיתוח","OrganizationName":"Microsoft","OrganizationDes":"Microsoft","OrganiztionImage":"ProfileImages\\Organizations\\Microsoft\\ProfileImage02_6_2014.png","OrgCity":"אבטליון"},
+        {"UserEmail":"carmela@harim.com","UserDes":"betaTester2","UserFname":"כרמלה","UserLname":"מנשה","UserAddress":"ההסתדרות 10","UserPhone":"054444666","ImagePath":"ProfileImages\\Users\\undefined\\ProfileImage09_5_2014.jpg","Gender":"M","Captain":false,"BirthDate":"1970-11-05","JoinDate":"2014-05-09","BicycleType":"הרים","RiderCity":"אבני חפץ","GroupName":"SecondGroup","GroupDes":"SecondGroup","OrganizationName":"EBAY","OrganizationDes":"EBAY","OrganiztionImage":"ProfileImages\\Organizations\\EBAY\\ProfileImage02_6_2014.png","OrgCity":"אבטליון"}] 
+         */
+        // api/Report?type=Users
         public HttpResponseMessage Post(string type, [FromBody]DataTable mlist)
         {
             try
-            {
-                int ColNum = 12;
+            {        
+                string[] hstr = { "Email", "שם משתמש", "שם פרטי", "שם משפחה", "ת.לידה", "ת.הצטרפות", "עיר", "קבוצה", "ארגון" };
+                int ColNum = hstr.Length;
                 string location = "~\\Reports\\" + type + "\\";
+                string slocation = "~\\sources\\reports\\" ;
                 string filename = type + DateTime.Now.ToString("dd_MM_yy_hh_mm_ss") + ".pdf";
                 var root = HttpContext.Current.Server.MapPath(location);
+                var sroot = HttpContext.Current.Server.MapPath(slocation);
 
                 Directory.CreateDirectory(root);
                 Document document = new Document(PageSize.A4);
                 PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(root + filename, FileMode.Create));
+
+                iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(sroot + "Logo.png");
+
+                document.Open();
+                document.NewPage();
+                BaseFont bfArialUniCode = BaseFont.CreateFont(sroot + "ariali.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+                Font font = new Font(bfArialUniCode, 10);
+                Font fontB = new Font(bfArialUniCode, 24);
+                // add logo
+                document.Add(logo);
+
                 PdfPTable table = new PdfPTable(ColNum);
-                BaseFont bf = BaseFont.CreateFont(root + "/arial.ttf", BaseFont.IDENTITY_H, true);
-                Font font = new Font(bf, 14);
-                //BaseFont unicode = BaseFont.CreateFont(root + "mriam.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-               // Font bold = new Font(unicode, 14);
-               // iTextSharp.text.Image logo = iTextSharp.text.Image.GetInstance(root + "Logo.jpg");
+                PdfPTable htable = new PdfPTable(ColNum);
 
-                table.WidthPercentage = 100;
-                //fix the absolute width of the table
-                table.LockedWidth = false;
-
-                //relative col widths in proportions - 1/3 and 2/3
-                float[] widths = new float[] { 6f, 6f, 9f, 9f, 9f, 9f, 7f, 9f, 9f, 9f, 9f, 9f};
-                table.SetWidths(widths);
+                table.DefaultCell.NoWrap = false;
                 table.HorizontalAlignment = 1;
-                //leave a gap before and after the table
-                table.SpacingBefore = 0f;
-                table.SpacingAfter = 0f;
-
-                PdfPCell cell = new PdfPCell(new Phrase(type));
-                cell.Colspan = ColNum;
-                cell.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
-                table.AddCell(cell);
-
+                table.LockedWidth = false;
+                table.WidthPercentage = 110;
                 
-                
-                // Fix the path to the font if needed
-                
-                /*ColumnText column = new ColumnText();
-                column.setSimpleColumn(36, 770, 569, 36);
-                column.setRunDirection(PdfWriter.RUN_DIRECTION_RTL);
-                String text = "הטקסט שלך בעברית"; // Your text in hebrew
-                column.addElement(new Paragraph(text, font));
-                column.go();
-                document.close();*/
-
-                foreach (DataColumn c in mlist.Columns)
+                //Create a regex expression to detect hebrew or arabic code points 
+                const string regex_match_arabic_hebrew = @"[\u0600-\u06FF,\u0590-\u05FF]+";
+                if (Regex.IsMatch("מה קורה", regex_match_arabic_hebrew, RegexOptions.IgnoreCase))
                 {
-                    if ((c.ColumnName == "BicycleType") || (c.ColumnName == "ImagePath") || (c.ColumnName == "Captain") || (c.ColumnName == "GroupName") || (c.ColumnName == "OrganiztionImage") || (c.ColumnName == "OrganizationName") || (c.ColumnName == "OrgCity"))
-                    { continue; }
-                    else
-                        table.AddCell(c.ColumnName);
+                    table.RunDirection = PdfWriter.RUN_DIRECTION_RTL;
+                    htable.RunDirection = PdfWriter.RUN_DIRECTION_RTL;
+                }
+                //Title
+
+                table.HorizontalAlignment = 1;
+                PdfPCell head = new PdfPCell(new Phrase(" רוכבים לעבודה - דו''ח רוכבים ", fontB));
+                head.Border = Rectangle.NO_BORDER;
+                head.Colspan = ColNum;
+                htable.AddCell(head);
+                htable.SpacingAfter = 20f;
+                document.Add(htable);
+                
+                // Headers
+                for (int x = 0; x < ColNum; x++)
+                {
+                    //Create a cell and add text to it 
+                    PdfPCell text = new PdfPCell(new Phrase(hstr[x], font));
+                    //Ensure that wrapping is on, otherwise Right to Left text will not display 
+                    text.NoWrap = false;
+
+                    //Add the cell to the table 
+                    table.AddCell(text);
                 }
 
-                int j = 1;
                 foreach (DataRow r in mlist.Rows)
                 {
                     int i = 0;
                     //table.AddCell(j.ToString());
                     foreach (DataColumn c in mlist.Columns)
                     {
-                        if ((c.ColumnName == "BicycleType") || (c.ColumnName == "ImagePath") || (c.ColumnName == "Captain") || (c.ColumnName == "GroupName") || (c.ColumnName == "OrganiztionImage") || (c.ColumnName == "OrganizationName") || (c.ColumnName == "OrgCity"))
+                        if ((c.ColumnName == "UserPhone") || (c.ColumnName == "UserAddress") || (c.ColumnName == "BicycleType") || (c.ColumnName == "ImagePath") || (c.ColumnName == "Gender") || (c.ColumnName == "Captain") || (c.ColumnName == "GroupName") || (c.ColumnName == "OrganiztionImage") || (c.ColumnName == "OrganizationName") || (c.ColumnName == "OrgCity"))
                         { i++; }
                         else
                         {
-                            table.AddCell(r.ItemArray[i].ToString());
+
+                            PdfPCell text = new PdfPCell(new Phrase(r.ItemArray[i].ToString(), font));
+                            //Ensure that wrapping is on, otherwise Right to Left text will not display 
+                            text.NoWrap = false;
+                            if (i == 1)
+                                text.Normalize();
+                            
+                            //Add the cell to the table 
+                            table.AddCell(text);
+                           // table1.AddCell(r.ItemArray[i].ToString());
                             i++;
                         }
                     }
-                    j++;
                 }
-
-                document.Open();
-                //document.Add(logo);
+                //Add the table to the document 
                 document.Add(table);
+                /* table.WidthPercentage = 100;
+                 //fix the absolute width of the table
+                 table.LockedWidth = false;
+
+                 //relative col widths in proportions - 1/3 and 2/3
+                 float[] widths = new float[] { 9f, 9f, 10f, 9f, 9f, 8f, 9f, 9f, 9f, 9f, 9f };
+                 table.SetWidths(widths);
+                 table.HorizontalAlignment = 1;
+                 //leave a gap before and after the table
+                 table.SpacingBefore = 0f;
+                 table.SpacingAfter = 0f;
+
+                 PdfPCell cell = new PdfPCell(new Phrase(type));
+                 cell.Colspan = ColNum;
+                 cell.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
+                 table.AddCell(cell);
 
 
+
+                 
+
+                 
+                
+                
+                 document.Add(table);
+                
+                 */
+                
 
                 document.Close();
+
+                Process AcrobatReader = new Process();
+                AcrobatReader.StartInfo.FileName = root + filename;
+                AcrobatReader.Start();
                 return Request.CreateResponse(HttpStatusCode.OK, root + filename);
 
             }
